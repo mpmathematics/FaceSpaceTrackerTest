fff85a7f0bdd8371f05acd8be4e20ee88d443de3
marcelPP <padilla.marcel@gmail.com>
2014-11-05 23:47:00 +0100

    14.11.05 commit test file

--- main.cpp
+++ main.cpp
@@ -41,6 +41,30 @@ FaceSpaceTracker myTracker("FaceSpaceTrackerDefault.cfg");
 
 int main(){
 
+	// you dont have to initialize anything here
+	// To get an understanding what is being tracked, set the following boolean true
+	bool showcamera = true;
+
+	// print out the log of the camera
+	myTracker.reportStatus();
+
+
+	// print info for what will happen if showcamera = true
+	if(showcamera){
+		std::cout << "Opening a new window to show the tracking input. " << std::endl;
+		std::cout << "This is for demonstration purpose only, it will slow down the program " << std::endl;
+		std::cout << "------ legend -------- " << std::endl;
+		std::cout << " Green rectangle: opencv highlighted face area " << std::endl;
+		std::cout << " Red rectangle: area where the face is expected in the next tracking" << std::endl;
+		std::cout << " Tiny black rectangle: estimated position of the eyes, this is the point being tracked " << std::endl;
+		std::cout << "----end legend -------- " << std::endl;
+		
+		// activate the camera window.
+		myTracker.setShowCamFrame(showcamera);
+	}
+
+		std::cout << " note: close the console to terminate this program " << std::endl;
+
 	// then start your main loop
 	while(true){
 		
@@ -48,18 +72,20 @@ int main(){
 		myTracker.updateTrackData();
 
 		// Now you can ask the tracker for the latest data
-		double xPosition = myTracker.getXHumanPosition();
-		double yPosition = myTracker.getYHumanPosition();
-		double zPosition = myTracker.getZHumanPosition();
+		double xPosition = myTracker.getXposition();
+		double yPosition = myTracker.getYposition();
+		double zPosition = myTracker.getZposition();
 
 		// lets print it
-		std::cout << " ( " << xPosition << " | " << yPosition << " | " << zPosition << " ) " << std::endl;
+		
+		std::cout << " ( x = " << xPosition << " | y = " << yPosition << " | z = " << zPosition << " ) " ;//<< std::endl;
+		std::cout << std::string(100, '\b');
 		
 		// do whatever you want with this data
 
 		//...
 
-		// now we send the program to sleep for 17 milliseconds to simulate the
+		// here we send the program to sleep for 17 milliseconds to simulate the
 		// waiting time of a programm that runs 60 loops per second,
 		// like in a smooth visualisation that runs 60 frames per second 
 		sleep(17);
